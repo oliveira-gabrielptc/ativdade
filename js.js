68142222a203a09052bfb047dd726112e1372887
oliveira-gabrielptc <129868237+oliveira-gabrielptc@users.noreply.github.com>
2023-10-09 16:16:25 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,16 @@
+var yanderu=[]
+for(var i=0;i<5;i++){
+    var preço=prompt("qual que é o preço")
+    var nome=prompt("qual que é nome")
+    var local=prompt("qual que o local de fabriçao")
+    var produto={
+        nome:nome,
+        preço:preço,
+        local:local
+
+    }
+ yanderu.push(produto)
+}
+for(var i=0;i<5;i++){
+    
+}
---
+++ main.js
@@ -0,0 +1,27 @@
+var jiko= prompt("qual operação")
+if (jiko=="3"){
+    var kijo=prompt("qual o primeiro o numero")
+    var sado=prompt("qual o segundo numero")
+    var quele=kijo/sado
+    alert("resutado:"+quele)
+}  
+if (jiko=="2"){
+    var kijo=prompt("qual o primeiro o numero")
+    var sado=prompt("qual o segundo numero")
+    var quele=kijo*sado
+    alert("resutado:"+quele)
+}  
+if (jiko=="1"){
+    var kijo=prompt("qual o primeiro o numero")
+    var sado=prompt("qual o segundo numero")
+    var quele=kijo-sado
+    alert("resutado:"+quele)
+}  
+if (jiko=="4"){
+    var kijo=parseInt(prompt("qual o primeiro o numero"))
+    var sado=parseInt(prompt("qual o segundo numero"))
+    var vim=parseInt(prompt("qual o terceiro numero"))
+    var skysong=parseInt(prompt("qual o quarto numero"))
+    var quele=kijo+sado+vim+skysong
+    alert("resutado:"+quele)
+}  
